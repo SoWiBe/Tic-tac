42011765fbdae20e94dfa412748b06c656215b0a
pr318gryzlov <fordd19_96@mail.ru>
2021-01-02 14:53:38 +0600

    Code with javafx started

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,9 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Tic-tac-toe");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.setResizable(false);
+
         primaryStage.show();
     }
 
