44eea695f3af03831f9b8277c416ab2a7addb1a6
pr318gryzlov <fordd19_96@mail.ru>
2021-01-03 16:14:43 +0600

    RadioBtn and ImageView completed)))

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -43,12 +43,69 @@ public class Controller {
     @FXML
     private ImageView imageNine;
 
+    private Gamer gamer;
+    private String motionOfGamer;
     @FXML
     void mainRegister(MouseEvent event) {
-        InputStream inputStream = getClass().getResourceAsStream("/resource/x.png");
-        Image image = new Image(inputStream);
-        imageOne.setImage(image);
+        InputStream inputStreamX = getClass().getResourceAsStream("/resource/x.png");
+        InputStream inputStreamO = getClass().getResourceAsStream("/resource/zero.png");
+
+        if(gamer.getMotion() == "X"){
+            Image imageX = new Image(inputStreamX);
+            getMotionMain(event.getSource(), imageX);
+        } else {
+            Image imageO = new Image(inputStreamO);
+            getMotionMain(event.getSource(), imageO);
+        }
+    }
+    void getMotionMain(Object object, Image image){
+        if(object == imageOne)
+            imageOne.setImage(image);
+        else if (object == imageTwo)
+            imageTwo.setImage(image);
+        else if (object == imageThree)
+            imageThree.setImage(image);
+        else if (object == imageFour)
+            imageFour.setImage(image);
+        else if (object == imageFive)
+            imageFive.setImage(image);
+        else if (object == imageSix)
+            imageSix.setImage(image);
+        else if (object == imageSeven)
+            imageSeven.setImage(image);
+        else if (object == imageEight)
+            imageEight.setImage(image);
+        else if (object == imageNine)
+            imageNine.setImage(image);
+    }
+    void clearAll(){
+        imageOne.setImage(null);
+        imageTwo.setImage(null);
+        imageThree.setImage(null);
+        imageFour.setImage(null);
+        imageFive.setImage(null);
+        imageSix.setImage(null);
+        imageSeven.setImage(null);
+        imageEight.setImage(null);
+        imageNine.setImage(null);
+    }
+    @FXML
+    void startingGame(){
+        gamer = new Gamer("Aleksey", motionOfGamer);
+    }
+    @FXML
+    void restartingGame(){
+        clearAll();
     }
 
+    @FXML
+    void setO(MouseEvent event) {
+        motionOfGamer = "O";
+    }
+
+    @FXML
+    void setX(MouseEvent event) {
+        motionOfGamer = "X";
+    }
 
 }
